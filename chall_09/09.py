cd6802188389ce88c1b0dd30aca0d4d2cd62201a
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-09 18:10:28 -0400

    #NOT FINISHED

--- chall_09/09.py
+++ chall_09/09.py
@@ -1,6 +1,6 @@
 from pwn import *
 binary = context.binary = ELF('./chall_09')
 p = process(binary.path)
-p.send(binary.string(binary.sym.key))
+p.send(xor(binary.string(binary.sym.key),b'\x69'))
 p.interactive()
 # NOT FINISHED
