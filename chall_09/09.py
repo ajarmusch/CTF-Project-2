a96f62067dea26de143581ffd3a89e911e0361f7
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 16:24:55 -0400

    # NOT FINISHED

--- chall_09/09.py
+++ chall_09/09.py
@@ -1,12 +1,11 @@
 from pwn import *
 binary = context.binary = ELF('./chall_09')
 p = process(binary.path)
-p.send()
 key = binary.string(binary.sym.key)
 x = xor(key,b'\x69')
 b = bytes.fromhex(x)
 c = xor(b, b'\x69')
-p.sendline(c)
+p.send(c)
 #string needs to be gotten manually you can set a break point at the 
 #key. You need to check the stack and get the key value
 p.interactive()
