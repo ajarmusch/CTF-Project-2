716b6c501715f0afe393a5ae9e0ebde4574c5958
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 16:26:25 -0400

    # NOT FINISHED

--- chall_09/09.py
+++ chall_09/09.py
@@ -2,8 +2,7 @@ from pwn import *
 binary = context.binary = ELF('./chall_09')
 p = process(binary.path)
 key = binary.string(binary.sym.key)
-x = xor(key,b'\x69')
-b = bytes.fromhex(x)
+b = bytes.fromhex(key)
 c = xor(b, b'\x69')
 p.send(c)
 #string needs to be gotten manually you can set a break point at the 
